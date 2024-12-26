4a606904901222a83a3e943382f7061d96f3843d
song <2906420727@qq.com>
2024-12-27 01:28:06 +0800

    提交

--- src/app/pages/home/components/News/News.tsx
+++ src/app/pages/home/components/News/News.tsx
@@ -5,8 +5,8 @@ export default function News() {
   return (
     <section className={styles.news}>
       <h2>关于我们</h2>
-      <ul className={styles.articles}>
-        <li className={styles.article}>
+      <div className={styles.articles}>
+        <div className={styles.article}>
           <div className={styles.articleHeader}>
             <span>NEWS</span>
             <hr />
@@ -14,8 +14,8 @@ export default function News() {
           <Image src="/images/home/news-1.png" alt="News 1" width={270} height={180} />
           <h3>How To Deliver a Successful Product Launch</h3>
           <p>29 Jun 2022, by Joshua Nash</p>
-        </li>
-        <li className={styles.article}>
+        </div>
+        <div className={styles.article}>
           <div className={styles.articleHeader}>
             <span>NEWS</span>
             <hr />
@@ -23,8 +23,8 @@ export default function News() {
           <Image src="/images/home/news-2.png" alt="News 2" width={270} height={180} />
           <h3>How To Deliver a Successful Product Launch</h3>
           <p>29 Jun 2022, by Joshua Nash</p>
-        </li>
-        <li className={styles.article}>
+        </div>
+        <div className={styles.article}>
           <div className={styles.articleHeader}>
             <span>NEWS</span>
             <hr />
@@ -32,8 +32,8 @@ export default function News() {
           <Image src="/images/home/news-3.png" alt="News 3" width={270} height={180} />
           <h3>How To Deliver a Successful Product Launch</h3>
           <p>29 Jun 2022, by Joshua Nash</p>
-        </li>
-        <li className={styles.article}>
+        </div>
+        <div className={styles.article}>
           <div className={styles.articleHeader}>
             <span>NEWS</span>
             <hr />
@@ -41,8 +41,8 @@ export default function News() {
           <Image src="/images/home/news-4.png" alt="News 4" width={270} height={180} />
           <h3>How To Deliver a Successful Product Launch</h3>
           <p>29 Jun 2022, by Joshua Nash</p>
-        </li>
-      </ul>
+        </div>
+      </div>
       <button className={styles.moreButton}>加载更多</button>
     </section>
   );
