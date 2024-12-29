be0d9667ff1aca1168406a126a253b2941ad1d7a
song <2906420727@qq.com>
2024-12-29 21:13:46 +0800

    提交

--- src/app/components/common/Footer/Footer.tsx
+++ src/app/components/common/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from "./Footer.module.css";
 
 export default function Footer() {
@@ -24,44 +25,44 @@ export default function Footer() {
           <div className={styles.column}>
             <h4>成为准父母</h4>
             <ul>
-              <li><a href="/#parents-overview">准父母概述</a></li>
-              <li><a href="/#surrogacy-matching-process">代孕母匹配过程</a></li>
-              <li><a href="/#parents-overview">试管医院的选择</a></li>
-              <li><a href="/#egg-sperm-donation-help">卵子和精子捐献者的帮组</a></li>
-              <li><a href="/#surrogacy-plan-process">代孕计划和流程</a></li>
-              <li><a href="/#becoming-parents">代孕套餐和费用</a></li>
+              <li><Link href="/#parents-overview">准父母概述</Link></li>
+              <li><Link href="/#surrogacy-matching-process">代孕母匹配过程</Link></li>
+              <li><Link href="/#parents-overview">试管医院的选择</Link></li>
+              <li><Link href="/#egg-sperm-donation-help">卵子和精子捐献者的帮组</Link></li>
+              <li><Link href="/#surrogacy-plan-process">代孕计划和流程</Link></li>
+              <li><Link href="/#becoming-parents">代孕套餐和费用</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h4>代孕母</h4>
             <ul>
-              <li><a href="#">补偿薪资</a></li>
-              <li><a href="#">代孕妈妈要求</a></li>
-              <li><a href="#">申请流程</a></li>
-              <li><a href="#">筛选过程</a></li>
-              <li><a href="#">经理历程</a></li>
+              <li><Link href="#">补偿薪资</Link></li>
+              <li><Link href="#">代孕妈妈要求</Link></li>
+              <li><Link href="#">申请流程</Link></li>
+              <li><Link href="#">筛选过程</Link></li>
+              <li><Link href="#">经理历程</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h4>卵子捐赠者</h4>
             <ul>
-              <li><a href="#">补偿薪资</a></li>
-              <li><a href="#">卵子捐赠者要求</a></li>
-              <li><a href="#">申请流程</a></li>
-              <li><a href="#">筛选过程</a></li>
-              <li><a href="#">经理历程</a></li>
+              <li><Link href="#">补偿薪资</Link></li>
+              <li><Link href="#">卵子捐赠者要求</Link></li>
+              <li><Link href="#">申请流程</Link></li>
+              <li><Link href="#">筛选过程</Link></li>
+              <li><Link href="#">经理历程</Link></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h4>资源</h4>
             <ul>
-              <li><a href="#">成为父母</a></li>
-              <li><a href="#">成为代孕妈妈</a></li>
-              <li><a href="#">成为捐赠者</a></li>
-              <li><a href="#">寻找捐赠者</a></li>
-              <li><a href="#">帮组</a></li>
-              <li><a href="#">联系我们</a></li>
-              <li><a href="#">咨询</a></li>
+              <li><Link href="#">成为父母</Link></li>
+              <li><Link href="#">成为代孕妈妈</Link></li>
+              <li><Link href="#">成为捐赠者</Link></li>
+              <li><Link href="#">寻找捐赠者</Link></li>
+              <li><Link href="#">帮组</Link></li>
+              <li><Link href="#">联系我们</Link></li>
+              <li><Link href="#">咨询</Link></li>
             </ul>
           </div>
         </div>
