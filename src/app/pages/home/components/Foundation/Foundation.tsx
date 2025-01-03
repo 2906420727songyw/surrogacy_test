198d05891de0986971394c5ad7021301195a1dcf
song <2906420727@qq.com>
2025-01-03 18:46:49 +0800

    提交

--- src/app/pages/home/components/Foundation/Foundation.tsx
+++ src/app/pages/home/components/Foundation/Foundation.tsx
@@ -4,8 +4,7 @@ export default function Foundation() {
   return (
     <section className={styles.foundation}>
       <div className={styles.content}>
-        <h2>Sapling Surrogacy</h2>
-        <p>0%利润投入基金会帮助多家庭圆梦</p>
+        <h2>0%利润投入基金会帮助更多家庭圆梦</h2>
         <div className={styles.buttons}>
           <button className={styles.donateButton}>了解更多</button>
           <button className={styles.applyButton}>捐款</button>
