97b79f5d47b117baa78f5237a8d5204e1d6f8f4c
song <2906420727@qq.com>
2025-01-07 23:20:17 +0800

    提交

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,13 +2,13 @@
 
 import { useEffect } from 'react';
 import http from '@/app/http';
-
+import {useState} from 'react';
 
 export default function TestPage() {
-   
+   const [ data, setData ] = useState([]);
     useEffect(() => {
-        http.get('/test').then((res) => {
-            console.log(res)
+        http.get('/view').then((res) => {
+            console.log(res.items)
         })
     }, []);
 
