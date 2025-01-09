ff75ca923820c7e30e7f97f228e5d37199cec154
song <2906420727@qq.com>
2025-01-09 18:02:13 +0800

    提交

--- src/app/pages/BecomeSurrogate/components/BecomeSurrogatePart2/BecomeSurrogatePart2.tsx
+++ src/app/pages/BecomeSurrogate/components/BecomeSurrogatePart2/BecomeSurrogatePart2.tsx
@@ -11,54 +11,64 @@ export default function BecomeSurrogatePart2() {
       layout="responsive" />
       <div className={styles.container}>
         <div className={styles.content}>
-          <h2 className={styles.title}>WHAT? 我们怎么筛选申请者</h2>
-          <p className={styles.description}>
+          <h2 className="text-3xl text-white mb-10 md:mb-20 md:text-5xl md:leading-tight">
+            WHAT? 我们怎么筛选申请者
+          </h2>
+          <p className="text-sm text-white leading-6 mb-10 md:text-xl md:leading-10 md:mb-11">
             根据美国生殖医学协会(ASRM)的指导要求,由专业的生殖医疗、心理专家、律师等<br/>
             第三方专业机构协助我们一起筛查申请者,具体涉及:
           </p>
         </div>
         <div className={styles.itemContainer}>
           <div className={styles.item}>
+            {/* 分割线 */}
             <div className={styles.divider}></div>
-            <p className={styles.itemTitle}>A</p>
-            <p className={styles.itemTitle}>身体健康评估</p>
-            <p className={styles.itemDetails}>
+            <p className="text-lg text-white mb-4 mt-6 md:text-xl md:mt-8 md:mb-6">
+              A<br/>
+              身体健康评估</p>
+            <p className="text-sm text-white text-center mb-6 md:text-lg md:mb-8">
               常规检查、生殖系统检查、传染病筛查、遗传病史、怀孕分娩史、杂交病史
             </p>
           </div>
           <div className={styles.item}>
             <div className={styles.divider}></div>
-            <p className={styles.itemTitle}>B</p>
-            <p className={styles.itemTitle}>心理健康评估</p>
-            <p className={styles.itemDetails}>
+            <p className="text-lg text-white mb-4 mt-6 md:text-xl md:mt-8 md:mb-6">
+              B<br/>
+              心理健康评估</p>
+            <p className="text-sm text-white text-center mb-6 md:text-lg md:mb-8">
             心理韧性的评估（情绪稳定、抗压能力、对代孕的理解和准备等）、代孕动机的评估、情感支持评估
             </p>
           </div>
           <div className={styles.item}>
             <div className={styles.divider}></div>
-            <p className={styles.itemTitle}>C</p>
-            <p className={styles.itemTitle}>法律背景审查</p>
-            <p className={styles.itemDetails}>
+            <p className="text-lg text-white mb-4 mt-6 md:text-xl md:mt-8 md:mb-6">
+              C<br/>
+              法律背景审查</p>
+            <p className="text-sm text-white text-center mb-6 md:text-lg md:mb-8">
               背景调查和无犯罪记录证明
             </p>
           </div>
           <div className={styles.item}>
             <div className={styles.divider}></div>
-            <p className={styles.itemTitle}>D</p>
-            <p className={styles.itemTitle}>健康和生活方式评估</p>
-            <p className={styles.itemDetails}>
+            <p className="text-lg text-white mb-4 mt-6 md:text-xl md:mt-8 md:mb-6">
+              D<br/>
+              健康和生活方式评估</p>
+            <p className="text-sm text-white text-center mb-6 md:text-lg md:mb-8">
               饮食和运动习惯、避免毒品和烟酒等、服用药物审查
             </p>
           </div>
           <div className={styles.item}>
             <div className={styles.divider}></div>
-            <p className={styles.itemTitle}>E</p>
-            <p className={styles.itemTitle}>身体适应性评估</p>
-            <p className={styles.itemDetails}>
+            <p className="text-lg text-white mb-4 mt-6 md:text-xl md:mt-8 md:mb-6">
+              E<br/>
+              身体适应性评估</p>
+            <p className="text-sm text-white text-center mb-6 md:text-lg md:mb-8">
               体重和BMI筛查、怀孕的身体准备
             </p>
           </div>
-          <button className={styles.button}>立即申请</button>
+          <button className="w-14 h-6 md:w-20 md:h-8 rounded text-xs md:text-sm font-medium text-black bg-[#cdc6c0] hover:bg-gray-100 transition duration-200 mt-10 mb-10 md:mt-20 md:mb-20">
+            立即申请
+          </button>
         </div>
       </div>
     </div>
