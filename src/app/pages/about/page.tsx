057b99de439771173048510446e8671a9ce3abb0
song <2906420727@qq.com>
2025-01-13 11:59:49 +0800

    提交

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -6,9 +6,9 @@ export default function AboutPage() {
   return (
     <main>
         <div className={styles.hero}>
-        <div className={styles.container}>
-          <h1 className={styles.heroTitle}>关于我们</h1>
-          <p className={styles.heroSubtitle}>
+        <div className="w-full">
+          <h1 className="text-xl text-white mb-10 md:text-4xl md:mb-20">关于我们</h1>
+          <p className="text-base text-white md:text-lg">
           假文案,这世界上有1/6的人因为个人原因,无法组成完整的家庭,<br/>
           他们梦想着能够抱着自己的孩子入睡,渴望看到宝宝在自己的呵护下长大。<br/>
           因为代孕妈妈无私的帮助,让这一切都变得有可能,代孕妈妈的无私奉献让更多有需要的人能够成为了父亲和母亲。<br/>
@@ -22,8 +22,12 @@ export default function AboutPage() {
             <article className={styles.newContainerContent}>
             <div className={styles.gradientBar}></div>
               <Image src={item.image} alt={`关于我们图片${index + 1}`} width={500} height={600} />
-              <h2 className={styles.newContainerTitle}>{item.title}</h2>
-              <p className={styles.newContainerText}>{item.content}</p>
+              <h2 className="text-lg text-white my-10 md:text-2xl md:my-14">
+                {item.title}
+              </h2>
+              <p className="text-sm text-white md:text-base">
+                {item.content}
+              </p>
             </article>
           </section>
         ))}
