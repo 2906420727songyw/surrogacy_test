60b1d06df3b0eb877868d63ed5bc8683ca34cd4c
loyal <loyaltly.cn@gmail.com>
2025-01-19 07:27:33 +0800

    update

--- src/app/pages/home/components/News/News.tsx
+++ src/app/pages/home/components/News/News.tsx
@@ -1,41 +1,38 @@
-
-import Image from 'next/image';
 import styles from './News.module.css';
 import { useRouter } from 'next/navigation';
-import { useRef ,useState,useEffect} from 'react';
-import http from '@/app/http';
-import { articlesData } from './data';
+import { useRef, useEffect } from 'react';
+import list from './data';
+
 export default function News() {
-  interface Article {
-    id: string;
-    imageUrl: string;
-    title: string;
-    description: string;
-    content: string;
-  }
   const router = useRouter();
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-  const [articles, setArticles] = useState<Article[]>([]);
-  const handleWheel = (event: React.WheelEvent) => {
-    if (scrollContainerRef.current) {
-      event.preventDefault();
-      scrollContainerRef.current.scrollLeft += event.deltaY;
-    }
-  };
-  useEffect(() => {
-    http.get('about').then((res) => {
-      //@ts-ignore
-      setArticles(res.items);
-    });
-  }, []);
 
-  const go_about = (id:string) => {
-    router.push('/pages/about');
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
+  const go_to_about = (index: number) => {
+    router.push('/pages/about')
+      setTimeout(()=>{
+        document.getElementById(`about-item-${index}`)?.scrollIntoView({ behavior: 'smooth' });
+    },500)
   }
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      const handleWheel = (event: WheelEvent) => {
+        const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+        if (
+          (event.deltaY < 0 && container.scrollLeft > 0) ||
+          (event.deltaY > 0 && container.scrollLeft < maxScrollLeft)
+        ) {
+          event.preventDefault();
+          container.scrollLeft += event.deltaY;
+        }
+      };
+
+      container.addEventListener('wheel', handleWheel);
+      return () => container.removeEventListener('wheel', handleWheel);
+    }
+  }, []);
 
   return (
     <section className={styles.news}>
@@ -43,31 +40,43 @@ export default function News() {
         关于我们
       </h2>
       <div
-        className="flex gap-10 mx-5 overflow-hidden justify-center"
+        className="flex gap-[5rem] mx-5 overflow-hidden"
         ref={scrollContainerRef}
-        onWheel={handleWheel}
       >
-        {articles.map((article) => (
-         <div className={`${styles.article} cursor-pointer w-60`} onClick={() => router.push('/pages/about')}>
-          <img className={styles.articleImage} src={article.imageUrl} alt={article.title}/>
-          <div className={styles.articleHeader}>
-            <span className="text-lg text-[#cdc6c0] mb-7.5 md:text-xl md:mb-5">
-              {article.title}
-            </span>
-            <div style={{ height: '10px' }}></div>
-            <hr />
-            <p className="text-xs text-[#cdc6c0] md:text-base">
-              {article.description}
-            </p>
-            <p className="text-xs text-[#cdc6c0] md:text-base">
-              {article.content.slice(0, 20)+'...'}
-            </p>
-            <hr />
+        {list.map((item, index) => (
+          <div
+            key={index}
+            className="cursor-pointer flex flex-col items-center gap-5"
+            onClick={() => go_to_about(index)}
+          >
+            <div className="w-[21rem] h-[28rem] overflow-hidden rounded-lg">
+              <img
+                className={styles.articleImage}
+                src={index < 2 ? 'https://loyal-cn.oss-ap-southeast-1.aliyuncs.com/macOS%20Monterey%20Wallpaper.jpg' : `/images/about/img/${index}.png`}
+                alt={item.name}
+              />
+            </div>
+            <div className={styles.articleHeader}>
+              <span className="flex flex-col gap-5 text-lg text-[#cdc6c0] mb-7.5">
+                {item.name}
+              </span>
+              {/* <div className={styles.spacing}></div> */}
+              <hr />
+              <p className="text-xs text-[#cdc6c0] md:text-base">
+                {item.role}
+              </p>
+              <p className="text-xs text-[#cdc6c0] md:text-base">
+                {item.content.toString().slice(0, 50) + '...'}
+              </p>
+              <hr />
+            </div>
           </div>
-        </div>
         ))}
       </div>
-      <button className="w-16 h-6 md:w-24 md:h-8 rounded text-xs md:text-sm font-medium text-black bg-[#cdc6c0] hover:bg-gray-100 transition duration-200 mt-10" onClick={() => router.push('/pages/about')}>
+      <button
+        className="w-16 h-6 md:w-24 md:h-8 rounded text-xs md:text-sm font-medium text-black bg-[#cdc6c0] hover:bg-gray-100 transition duration-200 mt-10"
+        onClick={() => router.push('/pages/about')}
+      >
         了解更多
       </button>
     </section>
