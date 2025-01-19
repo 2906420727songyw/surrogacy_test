521ca3d4a98621707cf05438463ac83585de8fb8
loyal <loyaltly.cn@gmail.com>
2025-01-20 03:35:07 +0800

    update

--- src/app/pages/home/components/AboutUs/videos.tsx
+++ src/app/pages/home/components/AboutUs/videos.tsx
@@ -1,14 +1,41 @@
+'use client';
+
 import React from 'react';
-import { Carousel } from 'antd';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
 
-const App: React.FC = () => (
-  <Carousel autoplay  className='mx-4 md:mx-48'>
-    {
-        Array.from({ length: 4 }).map((_, index) => (
-            <video className='rounded-lg my-10' src={`/videos/home/about-us-video-${index+1}.mp4`} key={index} controls></video>
-        ))
-    }
-  </Carousel>
-);
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
-export default App;
+export default function Test() {
+    
+  return (
+    <div>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView="auto"
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+          renderBullet: (index, className) => {
+            return `<span class="${className}" style="width: 12px; height: 12px; background-color: #ccc; margin: 0 5px; border-radius: 50%; display: inline-block;"></span>`;
+          },
+        }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+      >
+        {Array.from({ length: 10 }).map((item, index) => (
+          <SwiperSlide key={index} style={{ width: '80%', maxWidth: '1000px' }}>
+            <video
+              className='rounded-lg my-10'
+              src={`/videos/home/about-us-video-1.mp4`}
+              key={index}
+              controls
+            ></video>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
