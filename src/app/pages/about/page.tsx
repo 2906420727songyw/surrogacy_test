cbec6f42264dc34482ec3e8c376182055eda2aeb
loyal <loyaltly.cn@gmail.com>
2025-01-19 22:48:17 +0800

    update about order

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -23,8 +23,8 @@ export default function AboutPage() {
             <article className={styles.newContainerContent}>
             <div className={styles.gradientBar} id={`about-item-${index}`}></div>
               <img
-                  className={`${styles.articleImage} rounded-lg`}
-                  src={index < 2 ? 'https://loyal-cn.oss-ap-southeast-1.aliyuncs.com/macOS%20Monterey%20Wallpaper.jpg' : `/images/about/img/${index}.png`}
+                  className='rounded-lg'
+                  src={index > 2 ? 'https://loyal-cn.oss-ap-southeast-1.aliyuncs.com/macOS%20Monterey%20Wallpaper.jpg' : `/images/about/img/${index}.png`}
                   alt={item.name}
                 />
               <h2 className="text-lg text-white my-10 md:text-2xl md:my-14">
