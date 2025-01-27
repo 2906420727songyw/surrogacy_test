57e38ffff0828e264b46672453965a3b0f53b5a4
南风 <8785046+southwind8@user.noreply.gitee.com>
2025-01-27 17:44:35 +0800

    merge

--- src/app/pages/auth/profile/components/ProfileContent.tsx
+++ src/app/pages/auth/profile/components/ProfileContent.tsx
@@ -90,7 +90,7 @@ export default function ProfileContent() {
 
   const getDisplayValue = (value: string) => {
     if (!isClient) return '';
-    return value || (translations.language === 'EN' ? 'Not yet provided' : '暂未填写');
+    return value || (translations.language === 'EN' ?  '暂未填写' : 'Not yet provided');
   };
 
   if (isLoading) {
